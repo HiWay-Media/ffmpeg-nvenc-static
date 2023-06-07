6624e8e6daefe680d9e934393a6c9c390f8b3c1c
Allan Nava <allannava95@gmail.com>
2023-06-07 15:43:31 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,8 @@ apt-get install -y \
 #
 ## Install dependencies
 apt-get install -y \
-    autoconf automake build-essential cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libnuma-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libturbojpeg0-dev libvorbis-dev libx264-dev libxml2-dev libvpx-dev m4 make nasm ninja-build patch pkg-config tar zlib1g-dev autopoint imagemagick gsfonts wget
+    autoconf automake build-essential cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libnuma-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libturbojpeg0-dev libvorbis-dev libx264-dev libxml2-dev libvpx-dev m4 make nasm ninja-build patch pkg-config tar zlib1g-dev autopoint imagemagick gsfonts wget  libzmq5 libzmq3-dev
 #
-## Intel CSV dependencies
-#apt-get install -y libva-dev libdrm-dev
-#   
 #
 git clone -b sdk/$2 https://github.com/FFmpeg/nv-codec-headers.git \
 	&& cd nv-codec-headers \
@@ -47,6 +44,7 @@ cd ffmpeg-$1 \
     --enable-libopus \
     --enable-libvpx \
     --enable-libx264 \
+    --enable-libzmq \
     --extra-cflags=-I/usr/local/cuda/include \
     --extra-ldflags=-L/usr/local/cuda/lib64 \
  && make install \
